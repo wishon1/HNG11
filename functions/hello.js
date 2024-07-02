fb71f3975cd2fc550689a3240fd8424708a4c5b7
Wisdom Honest <117495032+wishon1@users.noreply.github.com>
2024-07-01 21:38:59 -0400

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -2,18 +2,33 @@ import express from 'express';
 import serverless from 'serverless-http';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+// Middleware to extract client IP address from request
 app.use((request, response, next) => {
     request.clientIp = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
     next();
 });
 
+/**
+ * @api {get} /api/hello Get greeting message
+ * @apiName GetGreeting
+ * @apiGroup Greeting
+ *
+ * @apiParam {String} visitor_name Visitor's name.
+ *
+ * @apiSuccess {String} clientIp The IP address of the requester.
+ * @apiSuccess {String} location The city of the requester.
+ * @apiSuccess {String} greeting Greeting message including the visitor's name and temperature.
+ *
+ * @apiError ServerError Server error message.
+ */
 app.get('/api/hello', async (request, response) => {
     try {
         const visitorName = request.query.visitor_name;
         const clientIp = request.clientIp;
 
-        const location = "New York";
+        const location = "Bayelsa";
         const temp = 11;
 
         const responseObj = {
@@ -29,5 +44,5 @@ app.get('/api/hello', async (request, response) => {
     }
 });
 
-module.exports = app;
+// Export the serverless handler
 module.exports.handler = serverless(app);
